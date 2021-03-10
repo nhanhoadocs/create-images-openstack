a810e69ce8de7ce0f0c4a6fa9b78c0568f4400b4
Đặng Đỗ Hải <danghai.hy.1996@gmail.com>
2021-03-10 16:42:49 +0700

    Create linux-login.sh

---
+++ scripts_all/linux-login.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo ""
+echo "Welcome to Cloud365 | nhanhoa.com"
+echo ""
+echo `date`
+echo ""
+echo "   ______ __                   __ _____  _____  ______"
+echo "  / ____// /____   __  __ ____/ /|__  / / ___/ / ____/"
+echo " / /    / // __ \ / / / // __  /  /_ < / __ \ /___ \  "
+echo "/ /___ / // /_/ // /_/ // /_/ / ___/ // /_/ /____/ /  "
+echo "\____//_/ \____/ \__,_/ \__,_/ /____/ \____//_____/   "
+echo ""
+echo "Trang chu NhanHoa : https://nhanhoa.com/"
+echo "Cloud365          : https://cloud365.vn/"
+echo "Portal            : https://portal.cloud365.vn/"
+echo "Huong dan su dung : https://support.cloud365.vn/"
+echo "Email ho tro      : support@nhanhoa.com"
+echo ""
+echo "*----------------------------------------------------*"
+echo ""
